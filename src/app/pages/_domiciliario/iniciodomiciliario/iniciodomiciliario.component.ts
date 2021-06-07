93426ad6cf8bb9f5494571536005d9c98457bd8a
ivaaan06 <62316002+ivaaan06@users.noreply.github.com>
2021-06-07 08:39:55 -0500

    Cierre de sesion funcional

--- src/app/pages/_domiciliario/iniciodomiciliario/iniciodomiciliario.component.ts
+++ src/app/pages/_domiciliario/iniciodomiciliario/iniciodomiciliario.component.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../../../environments/environment';
 import { Router } from '@angular/router';
 import { LoginService } from './../../../_service/login.service';
 import { Component, OnInit } from '@angular/core';
@@ -15,7 +16,10 @@ export class IniciodomiciliarioComponent implements OnInit {
   }
   cerrarSesion(){
     this.loginService.cerrarSesion().subscribe(data=>{
+      sessionStorage.setItem(environment.TOKEN, null);
+      sessionStorage.removeItem(environment.TOKEN);
       this.router.navigateByUrl('/login');
+    
     });
   }
 }
