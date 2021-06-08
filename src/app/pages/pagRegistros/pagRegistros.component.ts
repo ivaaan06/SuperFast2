589f92442bb184f4fbf5cb4bb310c00453b0ed61
lababativa <laubabativa.11@gmail.com>
2021-06-07 23:01:05 -0500

    registro aliado final

--- src/app/pages/pagRegistros/pagRegistros.component.ts
+++ src/app/pages/pagRegistros/pagRegistros.component.ts
@@ -12,75 +12,16 @@ import { RegistroService } from 'src/app/_service/registro.service';
 
 })
 export class PagRegistroComponent implements OnInit{
-    usuario = new Usuario() ;
-    des:string;
+    
     
     constructor(
-        private registroService: RegistroService,
-        private router: Router,
-        private _snackBar: MatSnackBar){
+       ){
 
     }
 
     ngOnInit(): void {
     }
 
-    guardarCambios(){
-        let nombre= ((document.getElementById("signin-name") as HTMLInputElement).value);
-        let apellido= ((document.getElementById("signin-lastname") as HTMLInputElement).value);
-        let correo= ((document.getElementById("signin-email") as HTMLInputElement).value);
-        let direccion= ((document.getElementById("signin-adress") as HTMLInputElement).value);
-        let telefono= ((document.getElementById("signin-number") as HTMLInputElement).value);
-        let contrasenia= ((document.getElementById("signin-pass") as HTMLInputElement).value);
-        let documento = ((document.getElementById("signin-document") as HTMLInputElement).value);
-    
-    
-        this.usuario.id_rol=1;
-        this.usuario.nombre = nombre;
-        this.usuario.apellido = apellido;
-        this.usuario.correo = correo;
-        this.usuario.contrasenia = contrasenia;
-        this.usuario.direccion = direccion;
-        this.usuario.telefono = telefono;
-        this.usuario.documento = documento;
-        this.usuario.imagenperfil= "imagen";
-        this.usuario.auditoria="papita";
-        this.usuario.aprobacion=15;
-          
-        //ejecutar servicio
-        this.registroService.barraProgreso.next("1");
-        this.registroService.registroUsuario(this.usuario).subscribe(data =>{
-          this.openSnackBar('Usuario registrado satisfactoriamente','Info');
-          this.registroService.barraProgreso.next("2");
-          this.router.navigate(['/login']);
-        }, err =>{
-          console.log(err);
-          this.openSnackBar("Capturar Error",'Error');
-        });
-          
-        
-      }
-      openSnackBar(message: string, action: string) {
-        this._snackBar.open(message, action, {
-            duration: 3000
-        });
-      }
-    
-    }
-    
-    export class MyErrorStateMatcher implements ErrorStateMatcher {
-      isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-        const isSubmitted = form && form.submitted;
-        return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-      }
-    }
-    
-    export class InputErrorStateMatcherExample {
-      emailFormControl = new FormControl('', [
-        Validators.required,
-        Validators.email,
-      ]);
-    
-      matcher = new MyErrorStateMatcher();
+   
 
 }
